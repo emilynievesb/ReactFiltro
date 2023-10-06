65327b2436007b6eb3b67e251bb60f60710365f7
Emily <nemily1710@gmail.com>
2023-10-06 08:25:51 -0500

    feat: :sparkles: Configuración de variables de entorno

---
+++ api/utils/config.js
@@ -0,0 +1,20 @@
+import { loadEnv } from "vite";
+
+const env = loadEnv("development", process.cwd(), "SERVER");
+const vite = loadEnv("development", process.cwd(), "VITE");
+
+const credentials = {
+  user: env.SERVER_USER,
+  pass: env.SERVER_PASSWORD,
+  secret: env.SERVER_PRIVATE_KEY,
+  db: env.SERVER_DATABASE,
+  hostname: env.SERVER_HOSTNAME,
+  port: env.SERVER_PORT,
+};
+
+const front = {
+  host: vite.VITE_HOST,
+  port: vite.VITE_PORT,
+};
+
+export { credentials, front };
