69e25b9d545241e6012e6bc14f9ef5dd26010c3e
Emily <nemily1710@gmail.com>
2023-10-06 09:23:21 -0500

    feat: :sparkles: Page productos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,20 @@
-import { Box } from "@mui/material";
+import { useState } from "react";
+import * as React from "react";
+import { Box, imageListClasses } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
+import { List } from "@mui/material";
 import Tooltip from "@mui/material/Tooltip";
-import * as React from "react";
-import { ListProducts } from "../components/ListProducts";
-import { useState } from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Typography from "@mui/material/Typography";
+import FolderIcon from "@mui/icons-material/Folder";
+import { productosFetch } from "../shared/services/fetchServices";
 import sideBar from "../img/sidebar.png";
 
 function Products() {
+  const [productos, setProductos] = React.useState([]);
+  const [cardProducto, setCardProducto] = React.useState({});
   const [state, setState] = useState({
     left: false,
   });
@@ -22,15 +29,29 @@ function Products() {
     setState({ ...state, [anchor]: open });
   };
 
+  React.useEffect(() => {
+    async function fetchData() {
+      const data = await productosFetch();
+      setProductos(data);
+    }
+    if (productos.length === 0) {
+      fetchData();
+    }
+  }),
+    [];
+
+  const handleClickProduct = (event) => {
+    const index = event.currentTarget.getAttribute("index");
+    setCardProducto(productos[index]);
+    console.log(cardProducto);
+  };
   return (
     <>
       <Box sx={{ width: "100%", display: "flex" }}>
         <Box
           sx={{
-            width: "50%",
+            width: "30%",
             height: "100vh",
-            overflowY: "auto",
-            borderRight: "1px solid #dcdcdc",
           }}
         >
           <div>
@@ -69,11 +90,44 @@ function Products() {
                   open={state[anchor]}
                   onClose={toggleDrawer(anchor, false)}
                 >
-                  <ListProducts />
+                  <List sx={{ padding: "1rem" }}>
+                    {productos.length !== 0
+                      ? productos.map((product, index) => (
+                          <ListItem
+                            key={index}
+                            index={index}
+                            onClick={handleClickProduct}
+                          >
+                            <ListItemIcon>
+                              <FolderIcon />
+                            </ListItemIcon>
+                            <Typography>
+                              Producto {Number(index) + 1}
+                            </Typography>
+                          </ListItem>
+                        ))
+                      : null}
+                  </List>
                 </Drawer>
               </React.Fragment>
             ))}
           </div>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+            height: "100vh",
+          }}
+        >
+          {Object.keys(cardProducto).length !== 0 ? (
+            <div>
+              <Typography variant="h5">{cardProducto.nombre}</Typography>
+              {cardProducto.imagen.map((imagen, index) => (
+                <img key={index} src={imagen} width={"40%"} />
+              ))}
+              <Typography variant="h5">{cardProducto.nombre}</Typography>
+            </div>
+          ) : null}
         </Box>
       </Box>
     </>
